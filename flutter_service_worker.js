bb48d2fe26756da016c5314f893b59a5a6d3c3e9
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2023-03-07 09:20:17 +0530

    New update on portfolio

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,8 +18,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "48b578dac57432d947bf8e68c6313cee",
-"/": "48b578dac57432d947bf8e68c6313cee",
+"index.html": "5f9bfebf5f533aacc837d04afbbc06f0",
+"/": "5f9bfebf5f533aacc837d04afbbc06f0",
 "main.dart.js": "939338bf718df780320d9dc48198afa1",
 "manifest.json": "eca88b55c92d231bddb7ea507ceb9220",
 "version.json": "ddf313f1432565846ca07c1037a4cde3"
