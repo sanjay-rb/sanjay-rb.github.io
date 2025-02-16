86e245bd86e64efe1e509cdbcab8bc2a3db6f16d
Sanjay Ramassery Babu <sanjaybabu618@gmail.com>
2025-02-16 21:41:49 +0530

    Feature/update project content (#10)
    * Update project version and timeline content with recent changes and bug fixes
    
    * Update project version to 2.1.3+3 in project_constant.dart
    
    * Update project timeline date and link in project_constant.dart

--- lib/app/constants/project_constant.dart
+++ lib/app/constants/project_constant.dart
@@ -4,65 +4,15 @@ import 'package:sanjayrb/app/data/models/timeline_model.dart';
 List<TimelineModel> project = [
   TimelineModel(
     title: "My Personal Portfolio",
-    subTitle: "Version : 2.1.0+2",
-    date: DateTime(2024, 07, 30),
+    subTitle: "Version : 2.1.3+3",
+    date: DateTime(2025, 02, 16),
     type: TimelineTypeConstant.project,
-    link: "NA",
+    link: "https://sanjay-rb.github.io/",
     points: [
-      'Added new version of Loga Parameshwari Temple app 🎉',
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.13.15+24",
-    date: DateTime(2024, 07, 05),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Minor update: Resolved image permission issue',
-      'Minor update: Enhanced Scheduled Pooja navigator',
-      'Minor update: Improved cloud function for notification text',
-      'Minor update: Integrated AdMob for mobile advertisements',
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.13.12+23",
-    date: DateTime(2024, 06, 18),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Minor update: Resolved image permission issue',
-      'Minor update: Enhanced Scheduled Pooja navigator',
-      'Minor update: Improved cloud function for notification text',
-      'Minor update: Integrated AdMob for mobile advertisements',
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.13.8+22",
-    date: DateTime(2024, 06, 09),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Security update : Google play policy fix',
-      'Minor revision : Updated special pooja style'
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.13.7+21",
-    date: DateTime(2024, 06, 08),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Bug fix: Permission issue fixed on storage',
-      'Minor revision : Updated securiy update on cloud functions',
-      'Minor revision : Updated special dates logic, map click'
+      '2.1.3+3: Updated content of timeline and fixed minor bugs. 🐞',
+      '2.1.0+2: Added new version of Loga Parameshwari Temple app 🎉',
+      '2.0.0+2: Revamped the entire app UI and functions for a smoother, more intuitive experience. 🚀',
+      '1.0.0+1: An portfolio page for my self to know more about me',
     ],
   ),
   TimelineModel(
@@ -78,16 +28,6 @@ List<TimelineModel> project = [
       'Personalized tracking of task completion with submissions via GitHub and LinkedIn posts. 📊',
       'Maintain your streak and stand out. 🌟',
       'By sharing task completions, you can network on LinkedIn and document your progress on GitHub. 🤝📚'
-    ],
-  ),
-  TimelineModel(
-    title: "My Personal Portfolio",
-    subTitle: "Version : 2.0.0+2",
-    date: DateTime(2024, 07, 20),
-    type: TimelineTypeConstant.project,
-    link: "NA",
-    points: [
-      'Revamped the entire app UI and functions for a smoother, more intuitive experience. 🚀',
     ],
   ),
   TimelineModel(
@@ -107,87 +47,50 @@ List<TimelineModel> project = [
     ],
   ),
   TimelineModel(
-    title: "My Personal Portfolio",
-    subTitle: "Version : 1.0.0+1",
-    date: DateTime(2023, 03, 09),
-    type: TimelineTypeConstant.project,
-    link: "NA",
-    points: [
-      'An portfolio page for my self to know more about me',
-    ],
-  ),
-  TimelineModel(
     title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.13.1+18",
-    date: DateTime(2023, 03, 6),
+    subTitle: "Version : 2.13.15+24",
+    date: DateTime(2024, 07, 05),
     type: TimelineTypeConstant.project,
     link:
         "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
     points: [
-      'Minor revision : Visibility of liked people names',
-      'Minor revision : User online status',
-      'Minor revision : History page lazy loading',
-      'Minor revision : User verification for new pooja addition',
-      'Minor revision : Permission handler',
-      'Minor revision : Contact cards',
-      'Minor revision : Special dates (Amavasay, Pournami & Ailyam Pooja Dates)',
-      'Minor revision : Android notification channel addition',
-      'Bug fix: Removed unwanted permissions'
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.5.8+16",
-    date: DateTime(2022, 11, 7),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Minor revision : In App Check in Cloud FireStore',
-      'Minor revision : Deeplink for pooja page',
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.5.4+14",
-    date: DateTime(2022, 05, 13),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Bug fix: 3d GIF link updated',
-      'Bug fix: Notification enable',
-      'Minor revision : Photo swipe features',
-      'Minor revision : Sponsors name list',
-      'Minor revision : Payment info page',
-      'Minor revision : Update date from the UI by the post creator (Edit Page)',
-      'Minor revision : App Tutorial'
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 2.0.0+10",
-    date: DateTime(2021, 07, 15),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Added authentication with mobile OTP.',
-      'Now you can add an image while scheduling the pooja.',
-      'In-app update checked.',
-      'New splash screen for preloading information.',
-      'New profile page for each user.'
-    ],
-  ),
-  TimelineModel(
-    title: "Loga Parameshwari Temple",
-    subTitle: "Version : 1.0.0+1",
-    date: DateTime(2021, 05, 27),
-    type: TimelineTypeConstant.project,
-    link:
-        "https://play.google.com/store/apps/details?id=com.sanjoke.loga_parameshwari",
-    points: [
-      'Initial version of the app',
+      '2.13.15+24: Minor update: Resolved image permission issue',
+      '2.13.15+24: Minor update: Enhanced Scheduled Pooja navigator',
+      '2.13.15+24: Minor update: Improved cloud function for notification text',
+      '2.13.15+24: Minor update: Integrated AdMob for mobile advertisements',
+      '2.13.12+23: Minor update: Resolved image permission issue',
+      '2.13.12+23: Minor update: Enhanced Scheduled Pooja navigator',
+      '2.13.12+23: Minor update: Improved cloud function for notification text',
+      '2.13.12+23: Minor update: Integrated AdMob for mobile advertisements',
+      '2.13.8+22: Security update : Google play policy fix',
+      '2.13.8+22: Minor revision : Updated special pooja style',
+      '2.13.7+21: Bug fix: Permission issue fixed on storage',
+      '2.13.7+21: Minor revision : Updated security update on cloud functions',
+      '2.13.7+21: Minor revision : Updated special dates logic, map click',
+      '2.13.1+18: Minor revision : Visibility of liked people names',
+      '2.13.1+18: Minor revision : User online status',
+      '2.13.1+18: Minor revision : History page lazy loading',
+      '2.13.1+18: Minor revision : User verification for new pooja addition',
+      '2.13.1+18: Minor revision : Permission handler',
+      '2.13.1+18: Minor revision : Contact cards',
+      '2.13.1+18: Minor revision : Special dates (Amavasay, Pournami & Ailyam Pooja Dates)',
+      '2.13.1+18: Minor revision : Android notification channel addition',
+      '2.13.1+18: Bug fix: Removed unwanted permissions',
+      '2.5.8+16: Minor revision : In App Check in Cloud FireStore',
+      '2.5.8+16: Minor revision : Deeplink for pooja page',
+      '2.5.4+14: Bug fix: 3d GIF link updated',
+      '2.5.4+14: Bug fix: Notification enable',
+      '2.5.4+14: Minor revision : Photo swipe features',
+      '2.5.4+14: Minor revision : Sponsors name list',
+      '2.5.4+14: Minor revision : Payment info page',
+      '2.5.4+14: Minor revision : Update date from the UI by the post creator (Edit Page)',
+      '2.5.4+14: Minor revision : App Tutorial',
+      '2.0.0+10: Added authentication with mobile OTP.',
+      '2.0.0+10: Now you can add an image while scheduling the pooja.',
+      '2.0.0+10: In-app update checked.',
+      '2.0.0+10: New splash screen for preloading information.',
+      '2.0.0+10: New profile page for each user.',
+      '1.0.0+1: Initial version of the app',
     ],
   ),
 ];
